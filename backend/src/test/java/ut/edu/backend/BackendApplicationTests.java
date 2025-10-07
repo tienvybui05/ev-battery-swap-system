8e636dcff6e1e4d046389f8b2abcde04bb8ac849
tienvybui05 <tienvy27052005@gmail.com>
2025-10-07 12:42:28 +0700

    Apply new .gitignore and clean tracked files

---
+++ backend/src/test/java/ut/edu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
