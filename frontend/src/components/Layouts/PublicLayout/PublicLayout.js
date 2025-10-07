a7d54f6065a43c6b1f880cc1ea9de0d8ea2f17bc
DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>
2025-10-07 16:24:10 +0700

    update layout of PublicLayout.js
    Signed-off-by: DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>

--- frontend/src/components/Layouts/PublicLayout/PublicLayout.js
+++ frontend/src/components/Layouts/PublicLayout/PublicLayout.js
@@ -1,14 +1,25 @@
 import { Outlet } from "react-router";
+import styles from "./PublicLayout.module.css";
+import GlobalStyles from "../../GlobalStyles/GlobalStyles.js";
 import Footer from "./Footer/Footer.js";
 import Header from "./Header/Header.js";
-import Main from "./Main/Main.js"
-function PublicLayout(){
-    return <>
-    <Header/>
-    <Main>
-         <Outlet />
-    </Main>
-    <Footer/>
+import Main from "./Main/Main.js";
+
+function PublicLayout() {
+  return (
+    <>
+      <GlobalStyles>
+        <div className={styles.wrapper}>
+          <Header />
+          <div className={styles.main}>
+            <Main>
+              <Outlet />
+            </Main>
+          </div>
+          <Footer />
+        </div>
+      </GlobalStyles>
     </>
+  );
 }
 export default PublicLayout;
