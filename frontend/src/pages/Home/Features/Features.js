c669fff37eba728f01b78dd6794dfc759fdac3e6
DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>
2025-10-09 10:21:57 +0700

    create Features for page home
    Signed-off-by: DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>

---
+++ frontend/src/pages/Home/Features/Features.js
@@ -0,0 +1,76 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationDot,
+  faBoltLightning,
+  faBatteryFull,
+  faUserShield,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "./Features.module.css";
+import vinfastLuxSa from "../../../assets/vinfastLuxSa.png";
+function Features() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <span>Tính năng mạnh mẽ</span>
+          <p>
+            CryptoFlow gives you the edge with advanced tools designed for both
+            beginners and professional traders.
+          </p>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.card}>
+            <FontAwesomeIcon
+              icon={faBoltLightning}
+              className={styles.faBoltLightning}
+            />
+            <span>Sạc Nhanh Tức Thì</span>
+            <p>
+              Sạc đầy pin trong vòng 20–30 phút với công nghệ sạc nhanh tiên
+              tiến – tiết kiệm thời gian cho mọi hành trình
+            </p>
+          </div>
+          <div className={styles.card}>
+            <FontAwesomeIcon
+              icon={faBatteryFull}
+              className={styles.faBatteryFull}
+            />
+            <span>Luôn Sẵn Sàng</span>
+            <p>
+              Sạc đầy pin trong vòng 20–30 phút với công nghệ sạc nhanh tiên
+              tiến – tiết kiệm thời gian cho mọi hành trình
+            </p>
+          </div>
+          <div className={styles.card}>
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              className={styles.faLocationDot}
+            />
+            <span>Phủ Sóng Toàn Quốc</span>
+            <p>
+              Hơn 30 trạm và đang mở rộng tại các thành phố lớn, trung tâm
+              thương mại và tuyến cao tốc
+            </p>
+          </div>
+          <div className={styles.card}>
+            <FontAwesomeIcon
+              icon={faUserShield}
+              className={styles.faUserShield}
+            />
+            <span>An Toàn & Tin Cậy</span>
+            <p>
+              Tất cả trạm sạc được kiểm định nghiêm ngặt, đảm bảo an toàn điện
+              và hiệu suất tối ưu cho xe của bạn
+            </p>
+          </div>
+        </div>
+        <img
+          src={vinfastLuxSa}
+          alt="Vinfast Lux SA"
+          className={styles.animationvinfastLuxSa}
+        />
+      </div>
+    </div>
+  );
+}
+export default Features;
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import AboutMe from "./AboutMe/AboutMe";
+import Features from "./Features/Features";
 import styles from "./Home.module.css";
 function Home() {
   return <div className={styles.wrapper}>
     <AboutMe/>
+    <Features/>
   </div>
 }
 export default Home;
